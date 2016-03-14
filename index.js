a8285e57df9f55acbd32a58a149279b4cb52284d
Francois Romain <francoisromain@gmail.com>
2016-03-14 16:09:42 +0100

    Improve bloc naming for something  more generic

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ module.exports = postcss.plugin('postcss-structure', function (options) {
         align: "center",
         type: "float"
     };
-    var sizes = ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl'];
     var r = postcss.root();
 
     return function (css) {
@@ -82,7 +81,7 @@ module.exports = postcss.plugin('postcss-structure', function (options) {
             columns.append({ prop: 'column-gap', value: opts.gutter + 'rem'});
             r.append(columns);  
 
-            for (a = 1; a <= sizes.length; a++) {
+            for (a = 1; a <= opts.max; a++) {
                 if (a < opts.min) {
                     totalWidth = opts.min * opts.width - opts.gutter;
                 } else if (a > opts.max) {
@@ -100,26 +99,26 @@ module.exports = postcss.plugin('postcss-structure', function (options) {
                 container.append({ prop: 'width', value: (totalWidth + 2 * opts.padding) + 'rem' });
                 media.append(container);
 
-                var show = postcss.rule({ selector: '.show-' + sizes[a - 1] });
+                var show = postcss.rule({ selector: '.show-' + a });
                 show.append({ prop: 'display', value: 'block !important' });
                 show.append({ prop: 'visibility', value: 'visible !important' });
                 media.append(show);
 
-                var floatRight = postcss.rule({ selector: '.' + sizes[a - 1] + '-right' });
+                var floatRight = postcss.rule({ selector: '.' + a + '-right' });
                 floatRight.append({ prop: 'float', value: 'right' });
                 media.append(floatRight);
 
                 var blocFloat = postcss.rule();
                 blocFloat.append({ prop: 'float', value: 'left' });
                 blocFloat.append({ prop: 'clear', value: 'none' });
-                for (col = opts.max; col > 0; col--) {
+                for (col = a; col > 0; col--) {
                     blocFloat.selector = blocFloat.selector ?
-                        blocFloat.selector + ', .bloc-' + sizes[a - 1] + '-' + col :
-                        '.bloc-' + sizes[a - 1] + '-' + col;
+                        blocFloat.selector + ', .bloc-' + a + '-' + col :
+                        '.bloc-' + a + '-' + col;
 
                     if (col > 1 && col < opts.max) {
                         for (off = 1; off <= opts.max - col; off++) {
-                            blocFloat.selector = blocFloat.selector + ', .bloc-' + sizes[a - 1] + '-' + col + '-' + off;
+                            blocFloat.selector = blocFloat.selector + ', .bloc-' + a + '-' + col + '-' + off;
                         }
                     }
                 }
@@ -134,7 +133,7 @@ module.exports = postcss.plugin('postcss-structure', function (options) {
                 }
 
                 for (b = 1; b <= a; b++) {
-                    for (var col = 1; col <= opts.max; col++) {
+                    for (var col = 1; col <= a; col++) {
                         var idx1 = false;
                         if (col >= a) {
                             idx1 = a;
@@ -142,8 +141,8 @@ module.exports = postcss.plugin('postcss-structure', function (options) {
                             idx1 = col;
                         }
                         if (idx1) {
-                            blocWidth[idx1].selector = blocWidth[idx1].selector ? blocWidth[idx1].selector + ', .bloc-' + sizes[b - 1] + '-' + col : '.bloc-' + sizes[b - 1] + '-' + col;
-                            columnCount[idx1].selector = columnCount[idx1].selector ? columnCount[idx1].selector + ', .columns-' + sizes[b - 1] + '-' + col : '.columns-' + sizes[b - 1] + '-' + col;
+                            blocWidth[idx1].selector = blocWidth[idx1].selector ? blocWidth[idx1].selector + ', .bloc-' + b + '-' + col : '.bloc-' + b + '-' + col;
+                            columnCount[idx1].selector = columnCount[idx1].selector ? columnCount[idx1].selector + ', .columns-' + b + '-' + col : '.columns-' + b + '-' + col;
                         }
                         
                         if (col > 1 && col < opts.max) {
@@ -157,8 +156,8 @@ module.exports = postcss.plugin('postcss-structure', function (options) {
                                     idx2 = 1;
                                 } 
                                 if (idx2) {
-                                    blocWidth[idx2].selector = blocWidth[idx2].selector ? blocWidth[idx2].selector + ', .bloc-' + sizes[b - 1] + '-' + col + '-' + off : '.bloc-' + sizes[b - 1] + '-' + col + '-' + off;
-                                    columnCount[idx2].selector = columnCount[idx2].selector ? columnCount[idx2].selector + ', .columns-' + sizes[b - 1] + '-' + col  + '-' + off : '.columns-' + sizes[b - 1] + '-' + col + '-' + off ;
+                                    blocWidth[idx2].selector = blocWidth[idx2].selector ? blocWidth[idx2].selector + ', .bloc-' + b + '-' + col + '-' + off : '.bloc-' + b + '-' + col + '-' + off;
+                                    columnCount[idx2].selector = columnCount[idx2].selector ? columnCount[idx2].selector + ', .columns-' + b + '-' + col  + '-' + off : '.columns-' + b + '-' + col + '-' + off ;
                                 }
                             }
                         }
